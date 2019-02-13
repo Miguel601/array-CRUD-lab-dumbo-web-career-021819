dd23c9ceefe75a11897e230c03db72d9e4aec3c4
Miguel Henriquez <henriquez.miguel@outlook.com>
2019-02-13 17:50:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  pokemon = ["pikachu", "Mew", "Mewtwo"]
+  pokemon = ["pikachu", "Mew", "Mewtwo", "togepi"]
 end
 
 def add_element_to_end_of_array(array, element)
