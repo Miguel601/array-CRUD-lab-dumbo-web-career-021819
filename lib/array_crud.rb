9206e7e7eb627553b3334fcf6d3ddda85454c4cc
Miguel Henriquez <henriquez.miguel@outlook.com>
2019-02-13 17:49:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  pokemon = []
 end
 
 def create_an_array
-  
+  pokemon = ["pikachu", "Mew", "Mewtwo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  pokemon << "scizor"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  pokemon.unshift("squirtle")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  pokemon.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  pokemon.shift
 end
 
 def retrieve_element_from_index(array, index_number)
